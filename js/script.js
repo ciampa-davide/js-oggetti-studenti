bcd0d4f791f9b61ed26ae050bb6308868ac43713
Davide <ciampad06@gmail.com>
2020-11-27 16:36:20 +0100

    Fine + fine snack andrea

--- js/script.js
+++ js/script.js
@@ -54,11 +54,40 @@ var insert = {
 }
 
 
-var name = promt("inserisci il nome dello studente");
-insert."nome".push(name);
+var name = prompt("inserisci il nome dello studente");
+insert.nome = name;
 
-var surname = promt("inserisci il cognome dello studente");
-insert."cognome".push(surname);
+var surname = prompt("inserisci il cognome dello studente");
+insert.cognome = surname;
 
 var age = prompt("inserisci l'eta' dello studente");
-insert."eta".push(age);
+insert.eta = age;
+
+console.log(insert);
+
+//___________________
+// js snack di andrea
+
+//crea un oggetto macchina chiedi tramite prompt alcuni dati
+// e poi stampala con un ciclo for
+
+
+var car = {
+  "nome": "",
+  "modello": "",
+  "targa": "",
+  "allestimento": "",
+}
+var carName= prompt("Inserisci il nome della macchina");
+var carModel = prompt("inserisci il modello");
+var carPlate = prompt("inserisci il numero di targa");
+var style = prompt("inserisci l'allestimento");
+
+
+car.nome = carName;
+car.modello = carModel;
+car.targa = carPlate;
+car.allestimento = style;
+
+
+console.log(car);
